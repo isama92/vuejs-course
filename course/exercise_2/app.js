9f7424aa0056758c8b49270022db102c6e937c5a
Stefano Borzoni <isama92@gmail.com>
2019-10-17 23:21:32 +0200

    exercise 2

--- course/exercise_2/app.js
+++ course/exercise_2/app.js
@@ -4,5 +4,9 @@ new Vue({
     el: '#exercise',
     data: {
         value: ''
-    }
+    },
+    methods: {
+        showAlert: () => alert('Hi, I am an alert!'),
+        saveInput: function(e) { this.value = e.target.value; },
+    },
 });
