8eb1a3b89a40c24c3a02e27cfdc859e3e84aa37d
Stefano Borzoni <isama92@gmail.com>
2019-10-20 17:45:19 +0200

    exercise 4

---
+++ course/exercise_4/app.js
@@ -0,0 +1,50 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        effectBool: false,
+        effectInterval: null,
+        selectClass: '',
+        selectClass2: '',
+        classOnOff: 'true',
+        inputStyle: '',
+        progress: 0,
+        maxProgress: 50,
+        progressInterval: null,
+    },
+    computed: {
+        effect: function() {
+            return {
+                highlight: this.effectBool,
+                shrink: !this.effectBool,
+            };
+        },
+        ex4: function() {
+            return {
+                [this.selectClass2]: true,
+                shrink: this.classOnOff === 'true',
+            };
+        },
+        progressPerc: function() {
+            // x : 100 = progress : maxprog
+            return Math.ceil(this.progress * 100 / this.maxProgress);
+        },
+    },
+    methods: {
+        startEffect: function() {
+            if(this.effectInterval)
+                clearInterval(this.effectInterval);
+
+            this.effectInterval = setInterval(() => {
+                this.effectBool = !this.effectBool;
+            }, 500);
+        },
+        startProgress: function() {
+            if(this.progressInterval !== null) return;
+
+            this.progressInterval = setInterval(() => {
+                if(++this.progress >= this.maxProgress)
+                    clearInterval(this.progressInterval);
+            }, 50);
+        },
+    }
+});
